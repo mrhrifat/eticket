2c4d1d10e1173786e57452c507d515b702256bb0
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-17 13:52:18 +0600

    buil: reducers of reserve & cancel history

--- src/redux.js
+++ src/redux.js
@@ -21,4 +21,22 @@ const cancelBooking = (name, refundAmount) => {
 
 // Reducers
 
+const reservationHistory = (oldReservationList = [], action) => {
+  if (action.type === "NEW_BOOKING") {
+    return [...oldReservationList, action.payload];
+  } else if (action.type === "CANCEL_BOOKING") {
+    return oldReservationList.filter((record) => {
+      return record !== action.payload.name;
+    });
+  }
+  return oldReservationList;
+};
+
+const cancellationHistory = (oldCancellationList = [], action) => {
+  if (action.type === "CANCEL_BOOKING") {
+    return [...oldCancellationList, action.payload];
+  }
+  return oldCancellationList;
+};
+
 // Redux Store
